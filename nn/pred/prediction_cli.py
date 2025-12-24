6168b5e2de78c28c26ac05bafd3686a26d6b258e
jeffreyHoelzel <jmh2338@nau.edu>
2025-12-23 17:36:40 -0700

    Refactored prediction CLI to use one fasta file as input to generate predictions

--- nn/pred/prediction_cli.py
+++ nn/pred/prediction_cli.py
@@ -9,12 +9,13 @@ from pathlib import Path
 import torch
 import esm
 import numpy as np
-from typing import Dict, Any, Tuple
+import pandas as pd
+from typing import Dict, Any, Tuple, Iterator, List
 from pipelineio.logger import setup_logger
 from esm2.embeddings import clean_seq, compute_window_embedding, append_seq_len
 from nn.models.ffnn import PepSeqFFNN
 
-CLASS_NAMES = ["Def epitope", "Uncertain", "Not eptiope"]
+CLASS_NAMES = ["Def epitope", "Uncertain", "Not epitope"]
 
 def find_peptide_start_stop(protein_seq: str, peptide: str) -> Tuple[int, int]:
     """
@@ -68,6 +69,33 @@ def infer_emb_dim(state: Dict[str, Any]) -> int:
         if torch.is_tensor(v) and v.dim() == 2:
             return int(v.shape[1])
     raise ValueError("Could not infer embedding dimension from model checkpoint, no 2D weight tensors found")
+
+def read_fasta_pep_prot(fasta_path: Path | str) -> Iterator[Tuple[str, str]]:
+    peptide = None
+    seq_lines = []
+    with open(fasta_path ,"r", encoding="utf-8") as fasta:
+        for raw in fasta:
+            line = raw.strip()
+
+            if not line:
+                continue
+
+            if line.startswith(">"):
+                if peptide:
+                    protein_seq = clean_seq("".join(seq_lines))
+                    pep = clean_seq(peptide)
+                    yield pep, protein_seq
+
+                peptide = line[1:].strip()
+                seq_lines = []
+            
+            else:
+                seq_lines.append(line)
+
+        if peptide:
+            protein_seq = clean_seq("".join(seq_lines))
+            pep = clean_seq(peptide)
+            yield pep, protein_seq
     
 def embed_protein_seq(protein_seq: str, 
                       model_name: str = "esm2_t33_650M_UR50D", 
@@ -121,30 +149,72 @@ def embed_protein_seq(protein_seq: str,
 
     return torch.from_numpy(rep_np)
 
+def predict(model: PepSeqFFNN, 
+            protein_seq: str, 
+            peptide: str, 
+            model_name: str, 
+            max_tokens: int, 
+            device: str) -> Dict[str, Any]:
+    protein_seq = clean_seq(protein_seq)
+    peptide = clean_seq(peptide)
+
+    align_start, align_stop = find_peptide_start_stop(protein_seq, peptide)
+        
+    # generate embedding and get peptide-level embedding
+    protein_emb = embed_protein_seq(protein_seq, model_name=model_name, max_tokens=max_tokens)
+    peptide_emb = protein_emb[align_start:align_stop, :]
+    X = peptide_emb.unsqueeze(0).to(device, non_blocking=True)
+
+    # generate probability outputs (predictions)
+    with torch.inference_mode():
+        logits = model(X) # (1, 3)
+        probs = torch.softmax(logits, dim=-1)[0].detach().cpu().tolist()
+
+    pred_idx = int(torch.tensor(probs).argmax().item())
+
+    return {"peptide": peptide, 
+            "protein": protein_seq, 
+            "p_def_epitope": probs[0], 
+            "p_uncertain": probs[1], 
+            "p_not_epitope": probs[2], 
+            "pred_class": CLASS_NAMES[pred_idx], 
+            "pred_index": pred_idx, 
+            "max_prob": max(probs)}
+
 def main() -> None:
     """Handles command-line argument parsing and high-level execution of the Prediction program."""
     parser = argparse.ArgumentParser(description="Predict epitope class for a peptide using a trained PepSeqPred model.")
     parser.add_argument("checkpoint", 
                         type=Path, 
                         help="Path to the trained model checkpoint (.pt).")
-    parser.add_argument("protein_seq",  
-                        type=str, 
-                        help="Full protein sequence the peptide came from.")
-    parser.add_argument("peptide", 
-                        type=str, 
-                        help="Peptide sequence to score.")
-    parser.add_argument("--align-start", 
+    parser.add_argument("fasta_input", 
+                        type=Path, 
+                        help="FASTA file where header is a peptide and body is a protein sequence.")
+    parser.add_argument("--output-csv", 
                         action="store", 
-                        dest="align_start", 
-                        type=int, 
-                        default=None, 
-                        help="0-based peptide start index in the protein.")
-    parser.add_argument("--align-stop", 
-                        action="store", 
-                        dest="align_stop", 
-                        type=int, 
-                        default=None, 
-                        help="0-based peptide stop index in the protein.")    
+                        dest="output_csv", 
+                        type=Path, 
+                        default=Path("predictions.csv"), 
+                        help="Output CSV path for predictions"
+                        )
+    # parser.add_argument("protein_seq",  
+    #                     type=str, 
+    #                     help="Full protein sequence the peptide came from.")
+    # parser.add_argument("peptide", 
+    #                     type=str, 
+    #                     help="Peptide sequence to score.")
+    # parser.add_argument("--align-start", 
+    #                     action="store", 
+    #                     dest="align_start", 
+    #                     type=int, 
+    #                     default=None, 
+    #                     help="0-based peptide start index in the protein.")
+    # parser.add_argument("--align-stop", 
+    #                     action="store", 
+    #                     dest="align_stop", 
+    #                     type=int, 
+    #                     default=None, 
+    #                     help="0-based peptide stop index in the protein.")    
     parser.add_argument("--model-name", 
                         action="store", 
                         dest="model_name", 
@@ -184,18 +254,18 @@ def main() -> None:
                           json_indent=json_indent, 
                           name="prediction_cli")
     
-    protein_seq = clean_seq(args.protein_seq)
-    peptide = clean_seq(args.peptide)
+    # protein_seq = clean_seq(args.protein_seq)
+    # peptide = clean_seq(args.peptide)
 
-    # get peptide start and stop indices
-    if args.align_start is None and args.align_stop is None:
-        align_start, align_stop = find_peptide_start_stop(protein_seq, peptide)
+    # # get peptide start and stop indices
+    # if args.align_start is None and args.align_stop is None:
+    #     align_start, align_stop = find_peptide_start_stop(protein_seq, peptide)
 
-    else:
-        align_start, align_stop = args.align_start, args.align_stop
+    # else:
+    #     align_start, align_stop = args.align_start, args.align_stop
 
-        if align_start < 0 or align_stop > len(protein_seq):
-            raise ValueError("Peptide not found as a contiguous substring in the provided protein sequence")
+    #     if align_start < 0 or align_stop > len(protein_seq):
+    #         raise ValueError("Peptide not found as a contiguous substring in the provided protein sequence")
     
     # load model from disk
     checkpt = torch.load(args.checkpoint, map_location="cpu")
@@ -212,40 +282,83 @@ def main() -> None:
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = model.to(device)
 
-    logger.info("prediction_start", 
+    logger.info("prediction_init", 
                 extra={"extra": {
-                    "protein_len": len(protein_seq),
-                    "peptide_len": len(peptide),
-                    "align_start": align_start,
-                    "align_stop": align_stop,
-                    "esm_model_name": args.model_name,
-                    "max_tokens": args.max_tokens,
-                    "emb_dim": emb_dim,
+                    "checkpoint": str(args.checkpoint), 
+                    "emb_dim": emb_dim, 
                     "device": device
                 }})
 
-    protein_emb = embed_protein_seq(protein_seq, 
-                                    model_name=args.model_name, 
-                                    max_tokens=args.max_tokens) # on CPU, (L, D + 1)
-    peptide_emb = protein_emb[align_start:align_stop, :]
-    X = peptide_emb.unsqueeze(0).to(device, non_blocking=True) # (1, L_p, D + 1)
+    # logger.info("prediction_start", 
+    #             extra={"extra": {
+    #                 "protein_len": len(protein_seq),
+    #                 "peptide_len": len(peptide),
+    #                 "align_start": align_start,
+    #                 "align_stop": align_stop,
+    #                 "esm_model_name": args.model_name,
+    #                 "max_tokens": args.max_tokens,
+    #                 "emb_dim": emb_dim,
+    #                 "device": device
+    #             }})
 
-    # generate probability outputs (predictions)
-    with torch.inference_mode():
-        logits = model(X) # (1, 3)
-        probs = torch.softmax(logits, dim=-1)[0].detach().cpu().tolist()
+    # handle fasta file input
+    rows: List[Dict[str, Any]] = []
+    n = 0
+    for pep, prot in read_fasta_pep_prot(args.fasta_input):
+        try:
+            row = predict(model, prot, pep, 
+                          args.model_name, args.max_tokens, device)
+            rows.append(row)
+            n += 1
 
-    pred_idx = int(torch.tensor(probs).argmax().item())
+            # log prediction progress every 10 peptides
+            if n % 10:
+                logger.info("prediction_progress", 
+                            extra={"extra": {
+                                "processed": n
+                            }})
+                
+        except Exception as e:
+            logger.error("prediction_failed", 
+                         extra={"extra": {
+                             "peptide": pep, 
+                             "error": str(e)
+                         }})
+    
+    # store as DataFrame for easy CSV file creation
+    df = pd.DataFrame(rows)
+    args.output_csv.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(args.output_csv, index=False)
 
-    output = {"align_start": align_start, 
-              "align_stop": align_stop, 
-              "peptide": peptide, 
-              "classes": CLASS_NAMES, 
-              "probs": probs, 
-              "pred_class": CLASS_NAMES[pred_idx], 
-              "pred_index": pred_idx}
+    logger.info("prediction_done", 
+                extra={"extra": {
+                    "output_csv": str(args.output_csv), 
+                    "processed": len(df), 
+                    "columns": list(df.columns)
+                }})
 
-    logger.info("prediction_done", extra={"extra": output})
+    # protein_emb = embed_protein_seq(protein_seq, 
+    #                                 model_name=args.model_name, 
+    #                                 max_tokens=args.max_tokens) # on CPU, (L, D + 1)
+    # peptide_emb = protein_emb[align_start:align_stop, :]
+    # X = peptide_emb.unsqueeze(0).to(device, non_blocking=True) # (1, L_p, D + 1)
+
+    # # generate probability outputs (predictions)
+    # with torch.inference_mode():
+    #     logits = model(X) # (1, 3)
+    #     probs = torch.softmax(logits, dim=-1)[0].detach().cpu().tolist()
+
+    # pred_idx = int(torch.tensor(probs).argmax().item())
+
+    # output = {"align_start": align_start, 
+    #           "align_stop": align_stop, 
+    #           "peptide": peptide, 
+    #           "classes": CLASS_NAMES, 
+    #           "probs": probs, 
+    #           "pred_class": CLASS_NAMES[pred_idx], 
+    #           "pred_index": pred_idx}
+
+    # logger.info("prediction_done", extra={"extra": output})
 
 if __name__ == "__main__":
     main()
